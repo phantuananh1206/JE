00cb2ac8a592113802c99eaa3a337fe8d33b2bb2
phantuananh1206 <phantuananhltt@gmail.com>
2022-10-14 00:16:14 +0700

    JE-4 UI-Header

---
+++ web/src/routes/index.js
@@ -0,0 +1,12 @@
+import Home from '~/pages/Home'
+
+// Public Routes
+const publicRoutes = [
+    { path: '/', component: Home }
+]
+
+const privateRoutes = [
+
+]
+
+export { publicRoutes, privateRoutes }
